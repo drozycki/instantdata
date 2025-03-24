d0ea0828c3689ec25cd3203263e718278fe9c74e
Daniel Rozycki <drozycki@users.noreply.github.com>
2025-03-23 17:15:55 -0700

    Fix indentation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,28 @@
 <script>
-	import { onMount, onDestroy } from 'svelte';
-	import { Chart, LineController, LineElement, PointElement, CategoryScale,
-    LinearScale, TimeScale, Decimation, Tooltip, } from 'chart.js';
+  import { onMount, onDestroy } from 'svelte';
+  import { Chart, LineController, LineElement, PointElement, CategoryScale,
+    LinearScale, TimeScale, Decimation, Tooltip, Title } from 'chart.js';
   import '$lib/chartjs-adapter-dayjs';
 
-	let canvas, hideText = true, worker;
-	Chart.register(LineController, LineElement, PointElement, CategoryScale,
-    LinearScale, TimeScale, Decimation, Tooltip);
+  let canvas, hideText = true, worker;
+  Chart.register(LineController, LineElement, PointElement, CategoryScale,
+    LinearScale, TimeScale, Decimation, Tooltip, Title);
 
-	onMount(async () => {
+  onMount(async () => {
     const SqliteWorker = (await import('$lib/sqliteWorker?worker')).default;
     worker = new SqliteWorker();
     worker.onmessage = async e => {
       if (e.data === 'ready') {
-        worker?.postMessage('SELECT date,aqi FROM aqi LIMIT 8;');
+        worker?.postMessage('SELECT date,aqi FROM aqi WHERE countyId = 81;');
       } else {
         new Chart(canvas, {
-    			type: 'line',
-		    	data: {
-    				datasets: [{
-		    			data: e.data.map(([x, y]) => ({x: x * 1000, y}))
-				    }]
-			    },
-			    options: {
+          type: 'line',
+          data: {
+            datasets: [{
+              data: e.data.map(([x, y]) => ({x: x * 1000, y}))
+            }]
+          },
+          options: {
             parsing: false,
             animation: {
               duration: 0,
@@ -36,6 +36,9 @@
             scales: {
               x: {
                 type: 'time', // 'linear'
+                time: {
+                  tooltipFormat: 'MMM D',
+                },
                 // min: 0,
                 // max: 1
               },
@@ -45,6 +48,10 @@
               }
             },
             plugins: {
+              title: {
+                display: true,
+                text: '2022 San Francisco Daily Air Quality Index'
+              },
               tooltip: {
                 enabled: true,
               },
@@ -57,12 +64,12 @@
         });
       }
     };
-	});
+  });
 
   onDestroy(() => worker?.terminate());
 </script>
 
 <div style="position: relative; width: 100vw; height: 100vh;">
-	<p style={hideText ? 'display: none;' : ''}>Loaded chart</p>
-	<canvas bind:this={canvas} />
+  <p style={hideText ? 'display: none;' : ''}>Loaded chart</p>
+  <canvas bind:this={canvas} />
 </div>
